8a6c6d37fab6e726ca1f28d045f3e7f4fa609749
lzgabel <lz19960321lz@gmail.com>
2022-12-11 12:50:24 +0800

    fix: keep colors consistent with Camunda Modeler

--- colors/ColorPopupProvider.js
+++ colors/ColorPopupProvider.js
@@ -2,31 +2,32 @@ import {
   domify
 } from 'min-dom';
 
-const COLORS = [ {
-  label: 'Default',
-  fill: undefined,
-  stroke: undefined
-}, {
-  label: 'Blue',
-  fill: '#BBDEFB',
-  stroke: '#0D4372'
-}, {
-  label: 'Orange',
-  fill: '#FFE0B2',
-  stroke: '#6B3C00'
-}, {
-  label: 'Green',
-  fill: '#C8E6C9',
-  stroke: '#205022'
-}, {
-  label: 'Red',
-  fill: '#FFCDD2',
-  stroke: '#831311'
-}, {
-  label: 'Purple',
-  fill: '#E1BEE7',
-  stroke: '#5B176D'
-} ];
+const COLORS = [
+  {
+    label: 'Default',
+    fill: undefined,
+    stroke: undefined
+  }, {
+    label: 'Blue',
+    fill: 'rgb(187, 222, 251)',
+    stroke: 'rgb(30, 136, 229)'
+  }, {
+    label: 'Orange',
+    fill: 'rgb(255, 224, 178)',
+    stroke: 'rgb(251, 140, 0)'
+  }, {
+    label: 'Green',
+    fill: 'rgb(200, 230, 201)',
+    stroke: 'rgb(67, 160, 71)'
+  }, {
+    label: 'Red',
+    fill: 'rgb(255, 205, 210)',
+    stroke: 'rgb(229, 57, 53)'
+  }, {
+    label: 'Purple',
+    fill: 'rgb(225, 190, 231)',
+    stroke: 'rgb(142, 36, 170)'
+  } ];
 
 
 export default function ColorPopupProvider(config, popupMenu, modeling, translate) {
